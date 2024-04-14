cee7352def112cb71a46ae99a0c9b11c26517303
markbarm <markbarm@gmail.com>
2024-04-14 08:48:20 -0300

    atualização

--- L0_04_[Repeticao] Astronauta confuso/Astronauta.py
+++ L0_04_[Repeticao] Astronauta confuso/Astronauta.py
@@ -1,5 +1,5 @@
 
-#função andar
+# FUNÇÃO ANDAR
 def andar(x,y,direcao,passos):
     if direcao%4 == 0:      
         y += passos
@@ -11,26 +11,33 @@ def andar(x,y,direcao,passos):
         x -= passos
     return x,y
 
-
-N,P = map(int,input().split())
-L = N*N # número de lugares na fila
-if P>L:
-    print(f"O astronauta ja saiu em missao ha {P-L} chamadas.")
-else:    
-    i = 0                   #índice da linha
-    j = -1                  #índice da coluna
-    P1 = P                  #contabiliza passos que irá dar para ocupar a posição P
+# FUNÇÃO POSIÇÃO FINAL
+def pos_final(steps,number):
+    x = 0                   #índice da linha
+    y = -1                  #índice da coluna
     d = 0                   #definir uma direção: 0 -> direita, 1 -> esquerda, 2-> cima, 3-> baixo
     teste = True            #controle para fechar o loop
     while teste:
-        if P1 <= N:
-            i,j = andar(i,j,d,P1)    
-            teste = False      
-        else:
-            i,j = andar(i,j,d,N)    
-            P1 -= N
+        if steps > number:
+            x,y = andar(x,y,d,number)    
+            steps -= number
             d += 1  
-            N -= d%2
+            number -= d%2 
+        else:
+            x,y = andar(x,y,d,steps)    
+            teste = False  
+    return x,y
+
+# Entrada do número de posições por lado, e posição do astronauta
+N,P = map(int,input().split())
+
+# Número de lugares na fila
+L = N*N 
+
+if P>L:
+    print(f"O astronauta ja saiu em missao ha {P-L} chamadas.")
+else:    
+    i,j = pos_final(P,N)
     if P<L:    
         print(f"O astronauta esta na posicao: {i} {j}\nAinda faltam {L-P} chamadas para a sua vez!")
     else:
