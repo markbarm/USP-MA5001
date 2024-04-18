2fc59a4e40b166b425ead9a062da8a24ea99c69c
markbarm <markbarm@gmail.com>
2024-04-18 17:46:38 -0300

    teste runcodes

---
+++ L1_10_[Estruturas de Controle] Divisiveis por 5 e 7/divisores.py
@@ -0,0 +1,9 @@
+# Entrada
+min, max = map(int,input().split(','))
+
+# Números divisíveis por 5 e 7 entre as entradas mínimo e máximo
+divisores=[]
+for n in range(min, max+1):
+    if n%35 == 0:
+        divisores.append(str(n))
+print(','.join(divisores))
