935725da4d0e9870eba52a3a4b8d50f8659e2f60
markbarm <markbarm@gmail.com>
2024-04-18 18:49:34 -0300

    runcodes

---
+++ L2_02_[Funcoes] Lista de Multiplos/multiplos.py
@@ -0,0 +1,14 @@
+def multiplos(m,M,i,j):
+    lista = []
+    for n in range(m, M+1):
+        if n%(x*y) == 0:
+            lista.append(str(n))
+    return print(','.join(lista))
+
+# Algoritmo
+min, max = map(int,input().split(','))
+x, y = map(int,input().split(','))
+multiplos(min, max,x,y)
+
+
+
