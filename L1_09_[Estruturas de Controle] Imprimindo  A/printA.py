28560d5b09e9d852c0da17af858b7ddfab4cd100
markbarm <markbarm@gmail.com>
2024-04-18 17:30:05 -0300

    correção

--- L1_09_[Estruturas de Controle] Imprimindo  A/printA.py
+++ L1_09_[Estruturas de Controle] Imprimindo  A/printA.py
@@ -1,10 +1,10 @@
 for i in range(7):
-    linha = []
+    linha = ''
     for j in range(7):
         if i in (0,3) and j in (2,3,4):
-            linha.append('*')
+            linha+='*'
         elif i in range(1,7) and j in (1,5):
-            linha.append('*')
+            linha+='*'
         else:    
-            linha.append(' ')
-    print(*linha)
+            linha+=' '
+    print(linha)
