11c57a5a6e0518d2e77cbbd319115703cd0ff92b
markbarm <markbarm@gmail.com>
2024-04-18 17:33:27 -0300

    identação

--- L1_09_[Estruturas de Controle] Imprimindo  A/printA.py
+++ L1_09_[Estruturas de Controle] Imprimindo  A/printA.py
@@ -2,9 +2,9 @@ for i in range(7):
     linha = ''
     for j in range(7):
         if i in (0,3) and j in (2,3,4):
-            linha+='*'
+            linha += '*'
         elif i in range(1,7) and j in (1,5):
-            linha+='*'
+            linha += '*'
         else:    
-            linha+=' '
+            linha += ' '
     print(linha)
