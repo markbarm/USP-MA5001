4be544d31b6133cc3dd5867851c168731dc1d931
Mark <markbarm@gmail.com>
2024-04-23 16:07:03 -0300

    Atualização

--- L2_06_[POO] Calculando Fracao/calculando_fracao.py
+++ L2_06_[POO] Calculando Fracao/calculando_fracao.py
@@ -1,38 +1,71 @@
 class Fracao(object):
 
-   def __init__(self, num,den):
+    def __init__(self, num,den):
        self.num=num
        self.den=den
        self.simplificar()
 
-   def __add__(self,outro):
+    def mdc(self):
+        num=int(self.num)
+        den=int(self.den)
+        resto = num % den
+        while(resto):
+        num=den
+        den=resto
+        resto=num%den
+        return den
+
+    def simplificar(self):
+        divisor= self.mdc()
+        self.num=int(self.num/divisor)
+        self.den=int(self.den/divisor)
+    
+    def soma(self,outro):
        somaDen=self.den*outro.den
        somaNum=outro.den*self.num + self.den*outro.num
-       if(somaNum%somaDen==0):
-           somaNum=somaNum//somaDen
-           somaDen=1
        return Fracao(somaNum,somaDen)
 
-   def __eq__(self,outro):
+    def subtracao(self,outro):
+       subDen=self.den*outro.den
+       subNum=outro.den*self.num - self.den*outro.num
+       return Fracao(subNum,subDen)
+    
+    def multiplicacao(self,outro):
+       multDen=self.den*outro.den
+       multNum=self.num*outro.num
+       return Fracao(multNum,multDen)
+    
+    def divisao(self,outro):
+       divDen=self.den*outro.num
+       divNum=self.num*outro.den
+       return Fracao(divNum,divDen)
+
+    def igualdade(self,outro):
        if(self.num/self.den==outro.num/outro.den):
            return True
        return False
 
-   def mdc(self):
-       num=int(self.num)
-       den=int(self.den)
-       resto = num % den
-       while(resto):
-        num=den
-        den=resto
-        resto=num%den
-       return den
-
-   def simplificar(self):
-       divisor= self.mdc()
-       self.num=int(self.num/divisor)
-       self.den=int(self.den/divisor)
-
-
-   def __str__(self):
+    def __str__(self):
        return str(self.num)+'/'+str(self.den)
+
+#Função que le a fração
+def input_fracao():
+    a, b = map(int, input().split('/'))
+    return Fracao(a,b)
+
+operacoes = {'+':'soma',
+             '-':'subtracao',
+             '*':'multiplicacao',
+             '/':'divisao',
+             '**':'potencia',
+             '==':'igualdade'}
+
+
+#Algoritmo 
+f1 = input_fracao()
+f2 = input_fracao()
+operacao = input()
+
+#Realiza a operação
+print(eval(f'f1.{operacoes[operacao]}(f2)'))
+
