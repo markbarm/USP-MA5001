f265cbca5133a87b418779dd708ea736bc2f4c28
markbarm <markbarm@gmail.com>
2024-04-26 04:05:36 -0300

    Planejamento de passos

--- L2_07_[POO] Classe  para funcionarios/funcionario.py
+++ L2_07_[POO] Classe  para funcionarios/funcionario.py
@@ -2,9 +2,29 @@
 
 class Funcionario(object):
 
-    def __init__(self,nome,sal,dep):
+    def __init__(self,nome,sal,dept):
         self.nome = nome
         self.sal = sal
-        self.dep = dep
+        self.dept = dept
     
+    #método get_nome
+    #método set_nome
+    #método get_salário
+    #método set_salário
+    #método set_departamento
+    #método __str__
+
+#Função 1. Recebe o nome e horas trabalhadas, retornando os dados do funcionário com o salário ajustado.
+
+#Função 2. Retorna o funcionário com maior salário
+
+#Função 3. Recebe o nome e novo departamento do funcionário, retornando os dados atualizados.
+
+#Função 4. Retorna a média salarial da lista de funcionários
     
+
+#Algoritmo
+    #recebe tamanho lista
+    #recebe lista de funcionários
+    #recebe solicitação
+    #executa solicitação
