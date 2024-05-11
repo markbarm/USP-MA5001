5f213c1e88cc6247f628df899b07f75824d1b751
markbarm <markbarm@gmail.com>
2024-05-10 21:20:59 -0300

    completo e testado

---
+++ P_sub_Q1_[5.0 pontos] Senha/senha.py
@@ -0,0 +1,21 @@
+def caracteres(lista):
+    
+    if len(lista)==1:
+        if lista[0].isupper():
+            return 'M' #Maiúsculo
+        elif lista[0].islower():
+            return 'm' #minúsculo
+        elif lista[0].isdigit():
+            return 'd' #dígito
+        else:
+            return '@' #não alfanumérico
+    else: 
+        return caracteres([lista.pop()]) + caracteres(lista)
+
+
+#algoritmo
+senha = input()
+if len(senha) >= 8 and len(set(caracteres(list(senha)))) >= 4:
+    print('Válida')
+else:
+    print('Inválida')
